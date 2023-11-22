797b897e0952ddde8c8a004f55e860d56d458e93
Lucas Giolo <giololucas@gmail.com>
2023-11-22 08:22:57 -0300

    início dos testes...

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import App from '../App';
+import { vi } from 'vitest';
+import { act } from 'react-dom/test-utils';
+import data from './data';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const URL = 'https://swapi.dev/api/planets';
+
+describe('Testa componente App', () => {
+
+  beforeEach(() => {    
+    vi.spyOn(global, 'fetch').mockResolvedValue({
+      status: 200,
+      ok: true,
+      json: async () => data,
+    } as Response);
+  });
+  
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+  test('Verifica ao renderizar o App se há o título e a mensagem "Carregando..." enquanto faz a requisição', async () => {
+    render(<App />);
+    await act(async () => {
+      global.fetch(URL);
+    });
+    // const loadingElement = screen.getByText(/Carregando.../i);
+    // expect(loadingElement).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(
+      () => screen.getByText(/Carregando.../i),
+      { timeout: 3500 },
+    );
+    // expect(loadingElement).not.toBeInTheDocument();
+    
+    expect(screen.getByText(/Star Wars Planets Search - Trybe/i)).toBeInTheDocument()
+    
+  });
+  
+})
