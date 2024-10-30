1f96d5c09c4bc61c4273809ada262fffe704ed34
Lucas Giolo <giololucas@gmail.com>
2024-10-29 21:22:09 -0300

    feat: Adiciona imagens e css global

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,31 @@ import planetsContext from './context/PlanetsContext';
 import FilterColumns from './Components/FilterColumns';
 import DisplayFilters from './Components/DisplayFilters';
 import FilterSortColumns from './Components/FilterSortColumns';
+import './global.css';
+import Title from './assets/title.svg';
 
 function App() {
   const { isLoading } = useContext(planetsContext);
   return (
     <>
-      <h1>Star Wars Planets Search - Trybe</h1>
-      {isLoading ? (
-        <h2>Carregando...</h2>
-      ) : (
-        <>
-          <InputSearchPlanets />
-          <div style={ { display: 'flex', flexDirection: 'row' } }>
-            <FilterColumns />
-            <FilterSortColumns />
-          </div>
-          <DisplayFilters />
-          <Table />
-        </>
-      )}
+      <header className="flex items-center justify-center">
+        <img src={ Title } alt="Star Wars Title" className="text-yellow-500" />
+      </header>
+      <main className="container">
+        {isLoading ? (
+          <h2>Carregando...</h2>
+        ) : (
+          <>
+            <InputSearchPlanets />
+            <div style={ { display: 'flex', flexDirection: 'row' } }>
+              <FilterColumns />
+              <FilterSortColumns />
+            </div>
+            <DisplayFilters />
+            <Table />
+          </>
+        )}
+      </main>
     </>
   );
 }
